d867c1c3a34a707dbdf24a1eab01fb20dcd394af
Beqa Lomadze <beqaweb@Beqas-MacBook-Pro.local>
2017-12-29 18:25:01 +0200

    initial commit

---
+++ filters/Normal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { GLSL, Shaders, Node } from 'gl-react';
+
+const shaders = Shaders.create({
+  Normal: {
+    frag: GLSL`
+      precision highp float;
+      varying vec2 uv;
+      uniform sampler2D inputImageTexture;
+      void main () {
+        vec3 texel = texture2D(inputImageTexture, uv).rgb;
+        gl_FragColor = vec4(texel, 1.0);
+      }`
+  }
+});
+
+const Normal = ({ children: t }) =>
+  (<Node
+    shader={shaders.Normal}
+    uniforms={{
+      inputImageTexture: t
+    }}
+  />);
+
+Normal.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default Normal;
